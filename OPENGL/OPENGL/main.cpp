a9413e39e304113f792ff05f64147c883921ade4
Donte Wilson <JittNA@aol.com>
2016-08-22 11:40:53 -0500

    OpenGL

---
+++ OPENGL/OPENGL/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "src\gl_core_4_4.h"
+#include <GLFW\glfw3.h>
+
+
+int main()
+{
+	if (glfwInit() == false)
+		return -1;
+
+	GLFWwindow* window = glfwCreateWindow(1280, 720,
+		"Computer Graphics",
+		nullptr, nullptr);
+
+	if (window == nullptr)
+	{
+		glfwTerminate();
+		return -2;
+	}
+
+	glfwMakeContextCurrent(window);
+	
+	if (ogl_LoadFunctions() == ogl_LOAD_FAILED)
+	{
+		glfwDestroyWindow(window);
+		glfwTerminate();
+		return -3;
+	}
+
+	auto major = ogl_GetMajorVersion();
+	auto minor = ogl_GetMinorVersion();
+	printf("GL: %i.%i\n", major, minor);
+
+	while (glfwWindowShouldClose(window) == false &&
+		glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS)
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	return 0;
+}
