c8554ac120ae2e54752bd666dfdebe3d10a84eec
Donte Wilson <JittNA@aol.com>
2016-08-28 18:47:55 -0500

    Comments and Removed things
    Edited code and comments

--- OPENGL/OPENGL/Solar.cpp
+++ OPENGL/OPENGL/Solar.cpp
@@ -1,6 +1,7 @@
 #include "Solar.h"
 #include <iostream>
 
+//sets a bool for create and if true then the function will run.
 bool Solar::Create()
 {
 	float time = (float)glfwGetTime();
@@ -8,8 +9,10 @@ bool Solar::Create()
 	if (glfwInit() == false)
 			return false;
 
+	//Sets window size to 1280x720 resolution
 	window = glfwCreateWindow(1280, 720, "Solar Application", nullptr, nullptr);
 
+	//If window nullptr then return the terminate to false.
 	if (window == nullptr)
 	{
 		glfwTerminate();
@@ -28,7 +31,7 @@ bool Solar::Create()
 	}
 
 
-
+	//Creates mat 4 objects that will be used to represent the planets.
 	Gizmos::create();
 
 	mat4 view = glm::lookAt(vec3(10, 10, 10), vec3(0), vec3(0, 1, 0));
@@ -39,15 +42,17 @@ bool Solar::Create()
 	mat4 moon;
 	mat4 jupiter;
 
+	//returns true
 	return true;
 }
+//Draws visuals to the screen. 
 void Solar::Draw()
-{
+{   //Sets the colors white and black
 	vec4 white(1);
 	vec4 black(0, 0, 0, 1);
 
 	for (int i = 0; i < 21; ++i)
-	{
+	{  //Creates lines to represent a 10x10 grid
 		Gizmos::addLine(vec3(-10 + i, 0, 10),
 			vec3(-10 + i, 0, -10),
 			i == 10 ? white : black);
@@ -56,12 +61,13 @@ void Solar::Draw()
 			vec3(-10, 0, -10 + i),
 			i == 10 ? white : black);
 	}
+	//Uses vec4 to create color values that will be used for the planets.
 	vec4 red(255, 0, 0, 1);
 	vec4 blue(0, 0, 255, 1);
 	vec4 orange(255, 128, 0, 1);
 	vec4 brownish(233, 150, 122, 1);
 
-
+	//Adds Spheres that will represent planets
 	Gizmos::addSphere(vec3(sun[3]), 1, 10, 10, orange, &sun);
 	Gizmos::addSphere(vec3(earth[3]), 0.75f, 10, 10, blue, &earth);
 	Gizmos::addSphere(vec3(venus[3]), 0.40f, 10, 10, red, &venus);
@@ -72,6 +78,7 @@ void Solar::Draw()
 	//Gizmos::addSphere();
 	//Gizmos::addSphere();
 
+	//draws in the resolution on the screen
 	Gizmos::draw(projection * view);
 
 	glfwSwapBuffers(window);
@@ -81,14 +88,16 @@ void Solar::Draw()
 }
 bool Solar::Update()
 {
+	//Sets float to 0
 	float angle = 0;
 
 	while (glfwWindowShouldClose(window) == false && glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS)
 	{
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+		//sets angle to equal pie divided by  180
 		angle += (3.14f / 180.f);
-
+		//Rotates planets around the sun uses vec3 math. 
 		sun = glm::rotate(angle, vec3(0, 1, 0));
 		jupiter = sun * glm::translate(vec3(9, 0, 3));
 		earth = sun * glm::translate(vec3(5, 0, 1));
@@ -97,13 +106,16 @@ bool Solar::Update()
 
 		Gizmos::clear();
 
+		//Adds Gizmos transform
 		Gizmos::addTransform(glm::mat4(1));
 
+		//returns true
 		return true;
 	}
 	return false;
 
 }
+//Terminate fucntion that will be used to destroy gizmos window.
 void Solar::Terminate()
 {
 	Gizmos::destroy();
@@ -111,7 +123,7 @@ void Solar::Terminate()
 	
 }
 
-bool Solar::Init()
-{
-
-}
+//bool Solar::Init()
+//{
+//
+//}
---
+++ OPENGL/OPENGL/src/Oldmain.cpp
@@ -0,0 +1,103 @@
+//Application * app = new Solar();
+//while (app->Init())
+//{
+
+//}
+
+//if (glfwInit() == false)
+//	return -1;
+
+//GLFWwindow* window = glfwCreateWindow(1280, 720,
+//	"Computer Graphics",
+//	nullptr, nullptr);
+
+//if (window == nullptr)
+//{
+//	glfwTerminate();
+//	return -2;
+//}
+
+//glfwMakeContextCurrent(window);
+
+//if (ogl_LoadFunctions() == ogl_LOAD_FAILED)
+//{
+//
+//	Gizmos::destroy();
+//	glfwDestroyWindow(window);
+//	glfwTerminate();
+//	return -3;
+//}
+
+//
+
+//Gizmos::create();
+
+//mat4 view = glm::lookAt(vec3(10, 10, 10), vec3(0), vec3(0, 1, 0));
+//mat4 projection = glm::perspective(glm::pi<float>() * 0.25f, 16 / 9.f, 0.1f, 1000.f);
+//mat4 sun;
+//mat4 earth;
+//mat4 venus;
+//mat4 moon;
+//mat4 jupiter;
+
+
+//auto major = ogl_GetMajorVersion();
+//auto minor = ogl_GetMinorVersion();
+//printf("GL: %i.%i\n", major, minor);
+
+//glClearColor(0.25f, 0.25f, 0.25f, 1);
+//glEnable(GL_DEPTH_TEST);
+//float angle = 0;
+
+//while (glfwWindowShouldClose(window) == false &&	glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS)
+//{
+//	float time = (float)glfwGetTime();
+
+//	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+//	angle += (3.14f/180.f);
+//	
+//	sun = glm::rotate(angle, vec3(0, 1, 0));
+//	jupiter = sun * glm::translate(vec3( 9, 0, 3));
+//	earth = sun * glm::translate(vec3(5, 0, 1));
+//	venus = sun * glm::translate(vec3(7, 0, 2));
+//	moon = earth * glm::translate(vec3(5, 0, 0));
+
+//	Gizmos::clear();
+
+//	Gizmos::addTransform(glm::mat4(1));
+
+//	vec4 white(1);
+//	vec4 black(0, 0, 0, 1);
+
+//	for (int i = 0; i < 21; ++i)
+//	{
+//		Gizmos::addLine(vec3(-10 + i, 0, 10),
+//			vec3(-10 + i, 0, -10),
+//			i == 10 ? white : black);
+
+//		Gizmos::addLine(vec3(10, 0, -10 + i),
+//			vec3(-10, 0, -10 + i),
+//			i == 10 ? white : black);
+//	}
+//	vec4 red(255, 0, 0, 1);
+//	vec4 blue(0, 0, 255, 1);
+//	vec4 orange(255, 128,0,1 );
+//	vec4 brownish(233, 150, 122, 1);
+//	
+
+//	Gizmos::addSphere(vec3(sun[3]), 1, 10, 10, orange, &sun);
+//	Gizmos::addSphere(vec3(earth[3]), 0.75f, 10, 10, blue, &earth);
+//	Gizmos::addSphere(vec3(venus[3]), 0.40f, 10, 10, red, &venus);
+//	Gizmos::addSphere(vec3(moon[3]), 0.65f, 10, 10, white, &moon);
+//	Gizmos::addSphere(vec3(jupiter[3]), 0.5f, 10, 10, brownish, &jupiter);
+
+//	//Gizmos::addSphere();
+//	//Gizmos::addSphere();
+//	//Gizmos::addSphere();
+
+//	Gizmos::draw(projection * view);
+
+//	glfwSwapBuffers(window);
+//	glfwPollEvents();
+//}
